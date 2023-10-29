702d05a49f426c61de451ff62f750c61db322e62
WiktorWielgusiak <witia16@gmail.com>
2023-10-29 16:51:06 +0100

    feature: handle signing out

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -11,9 +11,10 @@ import {
 import UserAvatar from "./UserAvatar";
 import { Session } from "next-auth";
 import { Button } from "./ui/button";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 
 function UserButton({ session }: { session: Session | null }) {
+  // Subscription listener...
   if (!session)
     return (
       <Button variant={"outline"} onClick={() => signIn()}>
@@ -27,12 +28,11 @@ function UserButton({ session }: { session: Session | null }) {
           <UserAvatar image={session.user?.image} name={session.user?.name} />
         </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => signOut()}>
+            Sign Out
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     )
