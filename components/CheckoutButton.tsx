03411e00c799557910a16989c633b94c91986618
WiktorWielgusiak <witia16@gmail.com>
2023-10-29 23:19:42 +0100

    feat: create registration page

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,5 +1,28 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 function CheckoutButton() {
-  return <div>Checkout Button</div>;
+  const { data: session } = useSession();
+  const createCheckoutSession = async () => {
+    // push a document into firestore db
+    // ...stripe extension on firebase will create a checkout session
+    // redirect user to checkout page
+  };
+  return (
+    <div>
+      {/* If subscribed show me the user is subscribed */}
+
+      <button
+        onClick={() => createCheckoutSession()}
+        className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 
+      text-white shadow-sm hover:bg-indigo-500 focus:visible:outline focus:visible:outline-2 focus:visible:outline-offset-2 
+      focus:visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white"
+      >
+        Sign Up
+      </button>
+    </div>
+  );
 }
 
 export default CheckoutButton;
