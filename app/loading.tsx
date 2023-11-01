ad7dacbb8c2937331f208c222d6fe9d3a389118e
WiktorWielgusiak <witia16@gmail.com>
2023-11-01 23:52:15 +0100

    feat: add loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import LoadingSpinner from "@/components/LoadingSpinner";
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="flex items-center p-10 justify-center">
+      <LoadingSpinner />
+    </div>
+  );
+}
+
+export default Loading;
