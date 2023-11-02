e6c90c2360ba7f49cbc44b883c970fc667a0f864
WiktorWielgusiak <witia16@gmail.com>
2023-11-02 16:22:03 +0100

    test: main test

--- components/SubscriptionProvider.tsx
+++ components/SubscriptionProvider.tsx
@@ -3,45 +3,31 @@
 import { subscriptionRef } from "@/lib/converters/Subscription";
 import { useSubscriptionStore } from "@/store/store";
 import { onSnapshot } from "firebase/firestore";
+import { useSession } from "next-auth/react";
 import { useEffect } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function SubscriptionProvider({ children }: { children: React.ReactNode }) {
+  const { data: session } = useSession();
   const setSubscription = useSubscriptionStore(
     (state) => state.setSubscription
   );
 
   useEffect(() => {
-    const auth = getAuth();
+    if (!session) return;
 
-    const authUnsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const snapshotUnsubscribe = onSnapshot(
-          subscriptionRef(user.uid),
-          (snapshot) => {
-            if (snapshot.empty) {
-              setSubscription(null);
-              return;
-            } else {
-              setSubscription(snapshot.docs[0].data());
-            }
-          },
-          (error) => {
-            console.error("Error fetching snapshot:", error);
-          }
-        );
-
-        return snapshotUnsubscribe;
-      } else {
-        setSubscription(null);
-      }
-    });
-
-    return () => {
-      authUnsubscribe();
-    };
-  }, [setSubscription]);
-
+    return onSnapshot(
+      subscriptionRef(session?.user.id),
+      (snapshot) => {
+        if (snapshot.empty) {
+          setSubscription(null);
+          return;
+        } else {
+          setSubscription(snapshot.docs[0].data());
+        }
+      },
+      (error) => {}
+    );
+  }, [session, setSubscription]);
   return <>{children}</>;
 }
 
